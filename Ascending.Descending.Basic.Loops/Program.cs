842ffa7f1d170c7503593bfc1a9da04297539ad3
Andrew <andrewpullen1@gmail.com>
2023-01-16 18:31:34 -0500

    First commit

---
+++ Ascending.Descending.Basic.Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿string answer = "y";
+
+do
+{
+    Console.Write("Enter a number: ");
+    int userNumber = int.Parse(Console.ReadLine());
+    for (int userNumberDecending = userNumber; userNumberDecending > -1; userNumberDecending--)
+    {
+        Console.Write(userNumberDecending + " ");
+    }
+    Console.WriteLine();
+    for (int zero = 0; zero < userNumber + 1; zero++)
+    {
+        Console.Write(zero + " ");
+    }
+    Console.WriteLine("\nWould you like to continue (y/n)?");
+    answer = Console.ReadLine().ToLower();
+} while (answer == "y");
+
+Console.WriteLine("\nGoodbye!");
